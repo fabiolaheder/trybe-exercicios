dc1464221add692f2ebca155ea11b111fcdf698a
fabiolaheder <fabiola3883@gmail.com>
2022-06-24 15:36:19 -0300

    adiciona os exercicios do bloco 4, dia 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio6.js
@@ -0,0 +1,17 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let count = 0;
+for(let index = 0; index < numbers.length; index +=1){
+    if (numbers[index] % 2 > 0){
+        count += 1;
+    }
+}
+
+
+if (count === 0){
+    console.log("nenhum valor ímpar encontrado");
+}else if(count > 1){
+    console.log(count + " " + "valores impares encontrados");
+}else{
+    console.log(count + " " + "valor impar encontrado")
+}
